f37642e99222c5a716b148d42966fce7af850e7f
Thi Ngoc Huyen Thai <475867@student.fontys.nl>
2025-06-14 21:07:05 +0200

    test

--- messaging-app/cypress/e2e/dashboard-search.cy.js
+++ messaging-app/cypress/e2e/dashboard-search.cy.js
@@ -1,20 +1,20 @@
-// describe('Dashboard - User Search', () => {
-//   beforeEach(() => {
-//     cy.login('huyen@gmail.com', 'Test123');
-//     cy.location('pathname').should('eq', '/dashboard');
-//   });
+describe('Dashboard - User Search', () => {
+  beforeEach(() => {
+    cy.login('huyen@gmail.com', 'Test123');
+    cy.location('pathname').should('eq', '/dashboard');
+  });
 
-//   it('searches users and shows results', () => {
-//     cy.get('input[placeholder="Enter username"]').type('Raoul');
-//     cy.contains('Search').click();
-//     cy.get('.search-btn').should('have.length.at.least', 1);
+  it('searches users and shows results', () => {
+    cy.get('input[placeholder="Enter username"]').type('Raoul');
+    cy.contains('Search').click();
+    cy.get('.search-btn').should('have.length.at.least', 1);
 
-//   });
+  });
 
-//   it('navigates to chat from search result', () => {
-//     cy.get('input[placeholder="Enter username"]').type('Raoul');
-//     cy.contains('Search').click();
-//     cy.get('.search-btn').first().click();
-//     cy.location('pathname').should('include', '/chat/');
-//   });
-// });
+  it('navigates to chat from search result', () => {
+    cy.get('input[placeholder="Enter username"]').type('Raoul');
+    cy.contains('Search').click();
+    cy.get('.search-btn').first().click();
+    cy.location('pathname').should('include', '/chat/');
+  });
+});
