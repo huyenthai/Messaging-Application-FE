fb2a7e1ead14962533e06ac76d286cb8f6f8060c
Thi Ngoc Huyen Thai <475867@student.fontys.nl>
2025-06-15 16:27:06 +0200

    test login

--- messaging-app/cypress/e2e/login.cy.js
+++ messaging-app/cypress/e2e/login.cy.js
@@ -2,6 +2,5 @@ describe('Login flow', () => {
   it('logs in with valid credentials', () => {
     cy.login('huyen@gmail.com', 'Test123');
     cy.visit('/dashboard');
-    cy.contains('Your Contacts');
   });
 });
